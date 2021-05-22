493f39c987320f523799f5033271f308b8fca2ab
Lalit Kumar <kalpitagoyal70@gmail.com>
2021-05-22 22:24:05 +0530

    made better

--- bot.py
+++ bot.py
@@ -6,11 +6,13 @@ from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pynse import *
 import json
+from flask import Flask, request
 
 logging.basicConfig(level=logging.DEBUG)
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
+server = Flask(__name__)
 
 bot = telebot.TeleBot(API_KEY)
 nse = Nse()
@@ -517,4 +519,20 @@ def option_chain(message):
         bot.send_message(message.chat.id, "Please enter proper details")
 
 
-bot.polling()
+@server.route('/' + API_KEY, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://nsepython.herokuapp.com/' + API_KEY)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
